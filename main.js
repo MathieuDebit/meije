a3449092b7fa44009bd4dcd88d7a9cf80ae79005
valentingalmand <Valentin@pc6.home>
2015-02-08 19:57:58 +0100

    Revert a23e4a0..d8968cd
    This rolls back to commit a23e4a078aa76ddcb41b1acc5d04817939e197ba.

--- main.js
+++ main.js
@@ -2,49 +2,47 @@
 
 var firstheight = window.innerHeight;
 var firstwidth = window.innerWidth;
-
 var game = new Phaser.Game(firstwidth, firstheight, Phaser.AUTO, 'gameDiv');
 
-// SCORE
-var timer;
-var total = 0;
-var highscore = 0;
-
-// RESPONSIVE
 $(window).resize(function () {
     display.resizer();
 });
 
 var display = {
+
     resizer: function () {
+
         var myheight = $(window).innerHeight();
         var mywidth = $(window).innerWidth();
+
         try {
+
             game.width = Number(mywidth);
             game.height = Number(myheight);
             game.stage.bounds.width = Number(mywidth);
             game.stage.bounds.height = Number(myheight);
+
             game.renderer.resize(Number(mywidth), Number(myheight));
+
             firstwidth = Number(mywidth);
             firstheight = Number(myheight);
+
             Phaser.Canvas.setSmoothingEnabled(game.context, false);
+
         } catch (e) {
+
             console.log("Error description: " + e.message + "");
+
         }
+
     }
 };
 
-<<<<<<< HEAD
 //var canvasH = 350,
 //    canvasW = 600;
 //var game = new Phaser.Game(canvasW, canvasH, Phaser.AUTO, 'gameDiv');
 
 var platforms;
-var speed = 1;
-=======
-// PLATEFORMS
-var platforms; 
->>>>>>> a23e4a078aa76ddcb41b1acc5d04817939e197ba
 
 var mainState = {
 
@@ -60,33 +58,25 @@ var mainState = {
         this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 
         // SOL
-        this.game.load.image('ground', 'assets/ground_mario.png');
+        this.game.load.image('ground', 'assets/ground.png');
 
         // OBSTACLES
-<<<<<<< HEAD
         this.game.load.image('rock', 'assets/rocks.png');
-        this.game.load.image('tronc', 'assets/tronc.png');
-=======
-        this.game.load.image('rock', 'assets/rock_mario.png');
->>>>>>> a23e4a078aa76ddcb41b1acc5d04817939e197ba
 
 
         // BACKGROUND
         this.game.load.image('vide', 'assets/empty.png');
-        this.game.load.image('back', 'assets/bg_01_mario.jpg');
-        this.game.load.image('back_02', 'assets/bg_02_mario.png');
-        this.game.load.image('back_03', 'assets/bg_03_mario.png');
-        this.game.load.image('back_04', 'assets/bg_04_mario.png');
+        this.game.load.image('back', 'assets/bg_01.jpg');
+        this.game.load.image('back_02', 'assets/bg_02.png');
+        this.game.load.image('back_03', 'assets/bg_03.png');
+        this.game.load.image('back_04', 'assets/bg_04.png');
 
         //SOUND
         this.game.load.audio('die', ['assets/trumpette.mp3', 'assets/trumpette.ogg']);
 
-<<<<<<< HEAD
-        console.log("firstWidth : " + firstwidth);
-        console.log("firstheight : " + firstheight);
+        console.log(firstwidth);
+        console.log(firstheight);
 
-=======
->>>>>>> a23e4a078aa76ddcb41b1acc5d04817939e197ba
     },
 
     ///////////////////////////////////////////////
@@ -99,11 +89,10 @@ var mainState = {
 
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        game.stage.backgroundColor = "70ccfd";
-        this.back = this.game.add.tileSprite(0, 0, firstwidth, 500, 'back');
-        this.back_04 = this.game.add.tileSprite(0, firstheight - 318 - 50, firstwidth, 318, 'back_04');
-        this.back_03 = this.game.add.tileSprite(0, firstheight - 254 - 50, firstwidth, 254, 'back_03');
-        this.back_02 = this.game.add.tileSprite(0, firstheight - 172 - 50, firstwidth, 172, 'back_02');
+        this.back = this.game.add.tileSprite(0, 0, firstwidth, 600, 'back');
+        this.back_04 = this.game.add.tileSprite(0, firstheight - 318, firstwidth, 318, 'back_04');
+        this.back_03 = this.game.add.tileSprite(0, firstheight - 254, firstwidth, 254, 'back_03');
+        this.back_02 = this.game.add.tileSprite(0, firstheight - 172, firstwidth, 172, 'back_02');
         this.vide = game.add.tileSprite(50, firstheight - 139, 'vide');
         this.ground = game.add.tileSprite(0, firstheight - 50, firstwidth, 91, 'ground');
 
@@ -119,9 +108,9 @@ var mainState = {
         //ROCKS
         this.rocks = game.add.group();
         this.rocks.enableBody = true;
-        this.rocks.createMultiple(50, 'rock');
+        this.rocks.createMultiple(10, 'rock');
 
-        this.timer = game.time.events.loop(3000, this.addRowOfrocks, this);
+        this.timer = game.time.events.loop(5000, this.addRowOfrocks, this);
 
 
         //PLAYER
@@ -134,20 +123,17 @@ var mainState = {
         this.dude.animations.add('space', [6], 10, true);
         this.dude.animations.add('stop', [4], 10, true);
 
-        //KEYS
+
+        //SPACEKEYKE
         this.spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-        this.upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
-        this.escapeKey = this.game.input.keyboard.addKey(Phaser.Keyboard.ESC);
+
 
         //DOUBLE SAUT
         this.compte = 0;
 
         //SCORE
-        score = game.time.create(false);
-        //score.loop(2000, updateCounter, this);
-        score.start();
 
-        
+
     },
 
     ///////////////////////////////////////////////
@@ -157,29 +143,17 @@ var mainState = {
     ///////////////////////////////////////////////
 
     update: function () {
-<<<<<<< HEAD
-        this.ground.tilePosition.x -= (3*speed);
-        this.back_04.tilePosition.x -= (0.2*speed);
-        this.back_03.tilePosition.x -= (0.4*speed);
-        this.back_02.tilePosition.x -= (0.8*speed);
-=======
-        
-        if (localStorage.getItem("hg")) {
-        highscore = localStorage.getItem("hg");
-    }
-        
         this.ground.tilePosition.x -= 3.3;
         this.back_04.tilePosition.x -= 0.2;
         this.back_03.tilePosition.x -= 0.4;
         this.back_02.tilePosition.x -= 0.8;
->>>>>>> a23e4a078aa76ddcb41b1acc5d04817939e197ba
 
         game.physics.arcade.collide(this.dude, platforms);
 
         // Double saut
         if (this.compte < 2) {
-            if (this.spaceKey.isDown || this.upKey.isDown) {
-                if (this.spaceKey.downDuration(5) || this.upKey.downDuration(5)) {
+            if (this.spaceKey.isDown) {
+                if (this.spaceKey.downDuration(5)) {
                     this.compte = this.compte + 1;
                     game.physics.arcade.enable(this.dude);
                     this.dude.body.gravity.y = 1000;
@@ -199,19 +173,13 @@ var mainState = {
             this.restartGame();
         }
 
+
         this.collides(this.rocks, this.dude)
-<<<<<<< HEAD
-        //game.physics.collide(this.dude, this.rock, collisionHandler, null, this);
-=======
->>>>>>> a23e4a078aa76ddcb41b1acc5d04817939e197ba
+            //game.physics.collide(this.dude, this.rock, collisionHandler, null, this);
 
-        // SCORE 
-
-        total++;
-        game.debug.text('Score : ' + total, 32, 32);
-        game.debug.text('HighScore : ' + highscore, 32, 32 * 1.5);
 
     },
+
 
     ///////////////////////////////////////////////
     //            
@@ -227,29 +195,32 @@ var mainState = {
         rock.reset(x, y);
 
         // Add velocity to the rock to make it move left
-        rock.body.velocity.x = -(180*speed);
+        rock.body.velocity.x = -200;
 
         // Kill the rock when it's no longer visible 
         rock.checkWorldBounds = true;
         rock.outOfBoundsKill = true;
         rock.body.immovable = true;
+
+
     },
 
     addRowOfrocks: function () {
-        var obstacleNumber = Math.floor((Math.random() * 3) + 1);
-        console.log("obstacleNumber : " + obstacleNumber);
-        console.log("speed : " + speed);
+        var obstacleNumber = Math.floor((Math.random() * 6) + 1);
         for (var i = 0; i < obstacleNumber; i++)
-<<<<<<< HEAD
-            this.addOnerock(i * (Math.floor((Math.random() * 170) + 130)) + firstwidth, firstheight - 72);
-            //speed = speed * 1.1;
-=======
-            this.addOnerock(i * 150 + firstwidth, firstheight - 82);
+            this.addOnerock(i * 150 + firstwidth, firstheight - 72);
 
         //this.addOnerock(0 + firstwidth, firstheight - 272);
->>>>>>> a23e4a078aa76ddcb41b1acc5d04817939e197ba
 
+        // Updated upstream
+        //
+        //                        for (var i = 0; i < 3; i++)
+        //                            if (i != hole && i != hole + 1)
+        //                                this.addOnerock(800 * i + firstwidth, firstheight - 72);
+        // Stashed changes
     },
+
+
 
     ///////////////////////////////////////////////
     //            
@@ -260,17 +231,6 @@ var mainState = {
     restartGame: function () {
         game.state.start('main');
         this.die.play();
-<<<<<<< HEAD
-        speed = 1;
-=======
-
-        if (total > highscore) {
-            highscore = total;
-            total = 0
-        }
-        
-        localStorage.setItem("hg", highscore);
->>>>>>> a23e4a078aa76ddcb41b1acc5d04817939e197ba
     },
 
     collides: function (a, b) {
